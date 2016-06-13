be4fafcb64dd42f6555fc64932982dbaf40b6594
Joseph Kachmar <joseph.kachmar@wework.com>
2016-06-13 11:18:05 -0400

    Comments 'event' and 'sensor' route modules

--- src/routes/sensor.js
+++ src/routes/sensor.js
@@ -4,14 +4,17 @@ import compose from 'koa-compose';
 
 import * as Ctrl from '../controllers/sensor';
 
+// Instantiates router for `/sensors` route
 const router = new Router({
   prefix: '/sensors',
 });
 
+// Binds controller functions to specific routes and HTTP methods
 router.get('/', Ctrl.getAllSensors);
 router.post('/', Ctrl.addSensor);
 router.put('/:uuid/location', Ctrl.updateLocation);
 
+// Boilerplate for easier error handling with `boom`
 const routes = router.routes();
 const allowedMethods = router.allowedMethods({
   throw: true,
