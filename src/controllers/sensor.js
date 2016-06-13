4f9578c67174f8a61efe044e8865154fd4180de8
Joseph Kachmar <joseph.kachmar@wework.com>
2016-06-13 11:15:09 -0400

    Comments 'controllers/sensor' module

--- src/controllers/sensor.js
+++ src/controllers/sensor.js
@@ -8,26 +8,29 @@ import {
   checkStatus,
 } from '../models/sensor';
 
+// Gets all sensor from database, responds with a JSON body
 export const getAllSensors = async (ctx) => {
   const sensor = await fetchAllSensors();
   ctx.body = sensor.toJSON();
 };
 
+// Adds a sensor and/or associated event information to the database
 export const addSensor = async (ctx) => {
   ctx.assert(ctx.request.type === 'application/json', 400);
   const body = ctx.request.body;
 
   ctx.assert(('id' in body), 400, 'Payload must contain "id" field.');
 
-  let sensor = await fetchOneSensor(body.id);
+  // Checks if the sensor exists...
+  const sensor = await fetchOneSensor(body.id);
   if (!sensor) {
+    // ... if it doesn't, insert it and block until finished
     await insertSensor(body.id);
-    sensor = await fetchOneSensor(body.id);
   }
 
+  // Validate JSON request body and insert sensor event if possible
   ctx.assert(('lp' in body), 400, 'Payload must contain "lp" field.');
   ctx.assert(('lb' in body), 400, 'Payload must contain "lb" field.');
-
   insertEvent(body);
 
   const pState = checkStatus(body.lp, sensor, 'paper_state');
@@ -38,11 +41,13 @@ export const addSensor = async (ctx) => {
     updateSensorState(body.id, pState, bState);
   }
 
+  // Respond with updated state for the sensor ID
   ctx.body = { uuid: body.id,
                paper_state: pState,
                battery_state: bState };
 };
 
+// Updates a sensor's location identifier
 export const updateLocation = async (ctx) => {
   const body = ctx.request.body;
   const sensorUuid = ctx.params.uuid;
