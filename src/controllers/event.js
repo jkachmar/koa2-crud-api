614e7e34c29dc249f9721554481db9772ff9f134
Joseph Kachmar <joseph.kachmar@wework.com>
2016-06-13 11:14:54 -0400

    Comments 'controllers/event' module

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -4,11 +4,13 @@ import {
 } from '../models/event';
 import { fetchOneSensor } from '../models/sensor';
 
+// Gets all sensor events from database, responds with a JSON body
 export const getAllEvents = async (ctx) => {
   const events = await fetchAllEvents();
   ctx.body = events.toJSON();
 };
 
+// Gets events for a specific sensor, responds with a JSON body or 400 code
 export const getSensorEvents = async (ctx) => {
   const sensorUuid = ctx.params.uuid;
   const sensor = await fetchOneSensor(sensorUuid);
